dc66b85b2bb1e72fe50e9a754757220f3eff5808
Umesh K <umeshkflance@gmail.com>
2018-12-13 11:28:48 +0530

    Add airbnb eslint configs and fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import WebFontLoader from 'webfontloader';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import WebFontLoader from 'webfontloader';
+import './index.scss';
 
 WebFontLoader.load({
   google: {
-    families: ['Roboto:300,400,500,700', 'Material Icons'],
-  },
+    families: ['Roboto:300,400,500,700', 'Material Icons']
+  }
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
